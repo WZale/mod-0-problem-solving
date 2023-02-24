b9e21b6fc4c511a53a482acad38f7dbc2299bc5a
William Zale <williamzale@gmail.com>
2023-02-24 14:53:37 -0500

    Initial commit

---
+++ challenge3.rb
@@ -0,0 +1,12 @@
+#Start with an array of strings. Print only the words that include the letter combination "ing".
+
+##Overall Goal##
+# The goal of this problem solving exercise is to print words from an array that include the letter combination "ing".
+
+##Pseudocode##
+# declare an array with strings containing words with and without the letter combination "ing"
+# use the .each do method to iterate through the array
+# use the .include? method to check for strings containing the letter combination "ing"
+# print the array
+
+##Final Solution##
